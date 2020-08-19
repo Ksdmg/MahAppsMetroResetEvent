4d483bad03afb41cfe9ccba3062300955898f91c
Alexander Schaaf <Alexander.Schaaf@INCOE.DE>
2020-08-19 16:56:05 +0200

    Add project files.

---
+++ Box/Box.xaml.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Threading
+Imports System.Windows.Threading
+
+Partial Public Class Box
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Public Overloads Shared Sub Show()
+
+
+        Using ready As New ManualResetEvent(False)
+            Dim BoxThread = New Thread(Sub()
+                                           Dim Box = New Box
+
+
+                                           Dim ClosedHandler As New EventHandler(Sub()
+                                                                                     RemoveHandler Box.Closed, ClosedHandler
+                                                                                     Box.Dispatcher.InvokeShutdown()
+                                                                                 End Sub)
+                                           AddHandler Box.Closed, ClosedHandler
+
+                                           Box.ShowDialog()
+                                           ready.Set()
+                                       End Sub)
+            BoxThread.Name = "WpfBox"
+            BoxThread.SetApartmentState(ApartmentState.STA)
+            BoxThread.IsBackground = True
+            BoxThread.Start()
+
+            ready.WaitOne()
+
+        End Using
+
+
+    End Sub
+
+
+End Class
---
+++ WpfTest/Application.xaml.vb
@@ -0,0 +1,12 @@
+﻿Class Application
+    Public Sub New()
+
+    End Sub
+
+    Private Sub Application_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+    End Sub
+
+    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
+    ' can be handled in this file.
+
+End Class
